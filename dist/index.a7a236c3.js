3c5f5c9259f16535714fa6ecbb11dcf5b84c1640
tobiaslrsn <nestorxxv@gmail.com>
2021-12-01 20:13:05 +0100

    end of day- push

--- dist/index.a7a236c3.js
+++ dist/index.a7a236c3.js
@@ -25,25 +25,26 @@ let todos: Article[
 ]; 
 
 */ window.onload = function() {
-};
-function productModal() {
     // Get the modal
-    let modal = document.getElementById("myModal");
-    // Get the <span> element that closes the modal
-    let span = document.getElementsByClassName("close")[0];
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
-    // Get all images and insert the clicked image inside the modal
-    // Get the content of the image description and insert it inside the modal image caption
-    let images = document.getElementsByClassName("product");
-    let captionText = document.getElementById("caption");
-    let i;
-    for(i = 0; i < images.length; i++)images[i].onclick = function() {
-        modal.style.display = "block";
-        captionText.innerHTML = this.innerHTML;
-    };
-}
+    productModal();
+    function productModal() {
+        let modal = document.getElementById("myModal");
+        // Get the <span> element that closes the modal
+        let span = document.getElementsByClassName("close")[0];
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        };
+        // Get all images and insert the clicked image inside the modal
+        // Get the content of the image description and insert it inside the modal image caption
+        let images = document.getElementsByClassName("product");
+        let captionText = document.getElementById("caption");
+        let i;
+        for(i = 0; i < images.length; i++)images[i].onclick = function() {
+            modal.style.display = "block";
+            captionText.innerHTML = this.innerHTML;
+        };
+    }
+};
 
 //# sourceMappingURL=index.a7a236c3.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -26,28 +26,28 @@ let todos: Article[
 
 */
 window.onload = function () {
-  productModal;
-};
-
-function productModal() {
   // Get the modal
-  let modal: any = document.getElementById("myModal");
-  // Get the <span> element that closes the modal
-  let span: any = document.getElementsByClassName("close")[0];
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
-  // Get all images and insert the clicked image inside the modal
-  // Get the content of the image description and insert it inside the modal image caption
-  let images: any = document.getElementsByClassName("product");
-  let captionText: any = document.getElementById("caption");
-  let i: number;
-  for (i = 0; i < images.length; i++) {
-    images[i].onclick = function () {
-      modal.style.display = "block";
+  productModal();
 
-      captionText.innerHTML = this.innerHTML;
+  function productModal() {
+    let modal: any = document.getElementById("myModal");
+    // Get the <span> element that closes the modal
+    let span: any = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
     };
+    // Get all images and insert the clicked image inside the modal
+    // Get the content of the image description and insert it inside the modal image caption
+    let images: any = document.getElementsByClassName("product");
+    let captionText: any = document.getElementById("caption");
+    let i;
+    for (i = 0; i < images.length; i++) {
+      images[i].onclick = function () {
+        modal.style.display = "block";
+
+        captionText.innerHTML = this.innerHTML;
+      };
+    }
   }
-}
+};
