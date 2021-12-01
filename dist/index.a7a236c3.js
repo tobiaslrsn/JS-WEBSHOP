d7f48a07aefa6458e396b77347a92007770bfce2
tobiaslrsn <nestorxxv@gmail.com>
2021-12-01 16:20:21 +0100

    some styling( push to pull)

--- dist/index.a7a236c3.js
+++ dist/index.a7a236c3.js
@@ -1,2 +1,31 @@
+/* class Article {
+  artist: string;
+  album: string;
+  year: number;
+  price: number;
+  description: string;
+  purchased: boolean;
+
+  constructor(
+    artist: string,
+    album: string,
+    year: number,
+    price: number,
+    description: string,
+    purchased: boolean
+  ) {
+    this.artist = artist;
+    this.album = album;
+    this.year = year;
+    this.price = price;
+    this.description = description;
+    this.purchased = purchased;
+  }
+}
+
+let todos: Article[
+
+]; */ window.onload = function() {
+};
 
 //# sourceMappingURL=index.a7a236c3.js.map
