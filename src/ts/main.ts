12e8a467020efeae5d33adef3131be9003930817
tobiaslrsn <nestorxxv@gmail.com>
2021-12-01 18:18:37 +0100

    modal - work in progress

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -26,4 +26,28 @@
 let todos: Article[
 
 ]; */
-window.onload = function () {};
+window.onload = function () {
+  // Get the modal
+  var modal: any = document.getElementById("myModal");
+
+  // Get the <span> element that closes the modal
+  var span: any = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // Get all images and insert the clicked image inside the modal
+  // Get the content of the image description and insert it inside the modal image caption
+  var images: any = document.getElementsByClassName("product");
+  var captionText: any = document.getElementById("caption");
+  var i;
+  for (i = 0; i < images.length; i++) {
+    images[i].onclick = function () {
+      modal.style.display = "block";
+
+      captionText.innerHTML = this.innerHTML;
+    };
+  }
+};
