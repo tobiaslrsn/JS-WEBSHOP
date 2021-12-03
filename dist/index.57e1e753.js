0c923f72d8002cecc7ad597e6bfd7c0b3fc371d1
malcolmlindberg <malcolm.lindberg@hotmail.se>
2021-12-03 12:43:32 +0100

    push to pull

--- dist/index.57e1e753.js
+++ dist/index.57e1e753.js
@@ -460,19 +460,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"9WuRb":[function(require,module,exports) {
 var _productObjects = require("./models/product-objects");
-<<<<<<< HEAD
-/* 
-let image001 = "src/assets/product-feed-img/1.jpg";
-let image002 = "src/assets/product-feed-img/2.jpg";
- */ let product001 = new _productObjects.Article("1.b18e163d.jpg", "Daft Punk", "Homework", 1997, 250, "DESCRIPTION", "Electronic", "purchase", "more info");
-let product002 = new _productObjects.Article("2.0fe79b40.jpg", "DIO", "Holy diver", 1983, 250, "DESCRIPTION", "rock", "purchase", "more info");
-let product003 = new _productObjects.Article("3.4e0a3919.jpg", "Queen", "New of the world", 1977, 250, "DESCRIPTION", "rock", "purchase", "more info");
-let product004 = new _productObjects.Article("4.a09afae4.jpg", "Calvin Harris", "I created disco", 1222, 250, "DESCRIPTION", "Electronic", "purchase", "more info");
-let product005 = new _productObjects.Article("5.ad46c5a6.jpg", "Avicii", "True", 2013, 250, "DESCRIPTION", "Electronic", "purchase", "more info");
-let product006 = new _productObjects.Article("6.692c53f5.jpg", "The Stone Roses", "the stone roses", 1989, 250, "DESCRIPTION", "rock", "purchase", "more info");
-let product007 = new _productObjects.Article("7.318e1685.jpg", "Destroyer", "Kaputt", 2011, 250, "DESCRIPTION", "rock", "purchase", "more info");
-let product008 = new _productObjects.Article("8.14acd5c6.jpg", "Bob Dylan", "Infidels", 1983, 250, "DESCRIPTION", "rock", "purchase", "more info");
-=======
 /* FRÅGOR:
 flera html-filer = ny mapp?
 ny scss för ny html fil, kamn man importera t.ex header och footer till den?
@@ -498,7 +485,6 @@ let product005 = new _productObjects.Article(image005, "Avicii", "True", 2013, 2
 let product006 = new _productObjects.Article(image006, "The Stone Roses", "the stone roses", 1989, 250, "DESCRIPTION", "rock", "purchase", "more info", "spotify");
 let product007 = new _productObjects.Article(image007, "Destroyer", "Kaputt", 2011, 250, "DESCRIPTION", "rock", "purchase", "more info", "spotify");
 let product008 = new _productObjects.Article(image008, "Bob Dylan", "Infidels", 1983, 250, "DESCRIPTION", "rock", "purchase", "more info", "spotify");
->>>>>>> feature/product-objects
 let products = [
     product001,
     product002,
