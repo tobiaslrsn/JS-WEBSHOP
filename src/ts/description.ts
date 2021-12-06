331f63fea95544a3dfaad9149c3f79424ce80d45
tobiaslrsn <nestorxxv@gmail.com>
2021-12-06 15:15:16 +0100

    wrong branch,

--- src/ts/description.ts
+++ src/ts/description.ts
@@ -5,10 +5,20 @@ window.onload = function () {
   /*   let heyhey = localStorage.getItem("product001");
   let hey: string = localStorage.getItem("product002");
  */
-  let productArtist: HTMLParagraphElement = document.createElement("p");
+  /*   let productArtist: HTMLParagraphElement = document.createElement("p");
   productArtist.className = "artist";
   productArtist.innerHTML = localStorage.getItem("product006"); //this.album
 
   document.body.appendChild(productArtist);
   console.log(productArtist);
+ */
+  let getobj: string = localStorage.getItem("product007");
+  let obj = JSON.parse(getobj);
+
+  let create = document.createElement("img");
+  create.className = "product-image";
+  create.innerHTML = obj.photo;
+
+  document.body.appendChild(create);
+  console.log(getobj);
 };
