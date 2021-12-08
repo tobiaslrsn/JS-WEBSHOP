db89d07136b7d50ddafa91fd48f8e9dd00281e84
malcolmlindberg <malcolm.lindberg@hotmail.se>
2021-12-08 10:49:50 +0100

    Cleaning up ts-code

--- dist/index.57e1e753.js
+++ dist/index.57e1e753.js
@@ -561,20 +561,13 @@ function productsToPage() {
 }
 function test() {
     console.log("Det funkar");
-    let purchaseBtnMalcolm = document.querySelectorAll(".purchase-button");
+    let purchaseBtnCount = document.querySelectorAll(".purchase-button");
     let cartAdding = document.getElementById("cartCount");
-    if (purchaseBtnMalcolm) {
+    if (purchaseBtnCount) {
         console.log('click');
-        // Kollar så element finns
         if (cartAdding) {
-            // CartCount är antingen odefinierat eller ett nummer
-            // Om det är undifined faller det tillbaka till 0. Konvertera
-            // till int eftersom value från innerText
-            // returnerar en string;
             let cartCount = Number(cartAdding.innerText || 0);
-            // Ökar med ett efter varje click
             cartAdding.innerText = String(cartCount + 1);
-            //Gör att amount kommer upp
             cartAdding.style.visibility = "visible";
         }
     }
--- src/ts/products.ts
+++ src/ts/products.ts
@@ -219,23 +219,14 @@ export function productsToPage() {
 
 function test(){
   console.log("Det funkar")
-    let purchaseBtnMalcolm = document.querySelectorAll(".purchase-button")
+    let purchaseBtnCount = document.querySelectorAll(".purchase-button");
     let cartAdding: HTMLButtonElement = document.getElementById("cartCount") as HTMLButtonElement;
 
-
-    if (purchaseBtnMalcolm) {
+    if (purchaseBtnCount) {
           console.log('click');
-          // Kollar så element finns
           if (cartAdding) {
-            // CartCount är antingen odefinierat eller ett nummer
-            // Om det är undifined faller det tillbaka till 0. Konvertera
-            // till int eftersom value från innerText
-            // returnerar en string;
             let cartCount = Number(cartAdding.innerText || 0);
-      
-            // Ökar med ett efter varje click
             cartAdding.innerText = String(cartCount + 1);
-            //Gör att amount kommer upp
             cartAdding.style.visibility ="visible"
         }
         
